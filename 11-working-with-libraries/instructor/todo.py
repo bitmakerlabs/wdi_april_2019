b27da5b3782cd6dacd1755a4927e14199752cd88
NatalieBlack <natalievictoriablack@gmail.com>
2019-05-13 12:05:34 -0400

    In-class code for libraries lesson

--- 11-working-with-libraries/instructor/todo.py
+++ 11-working-with-libraries/instructor/todo.py
@@ -1,15 +1,21 @@
+# let user specify due date for item
+  # datetime.date class
+# display how much time is left until it's due
+  # today() looks useful
+# e.g. _____ due in _____ days
+import arrow
+import datetime
 class ToDo:
   """ Represents an item on a to-do list """
 
-  def __init__(self, description):
+  def __init__(self, description, due_date):
     self.description = description
     self.completed = False
+    self.due_date = due_date 
 
   def complete(self):
     self.completed = True
 
   def __str__(self):
-    return "To do: {}".format(self.description)
+    return "To do: {} - due {}".format(self.description, self.due_date.humanize())
 
-todo = ToDo('buy groceries')
-print(todo)

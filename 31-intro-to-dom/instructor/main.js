cdf0ae88cb7e04feff6bee3aeadd0b56445e6fab
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2019-06-17 12:12:18 -0400

    In class code for DOM lecture

--- 31-intro-to-dom/instructor/main.js
+++ 31-intro-to-dom/instructor/main.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(e){
   // CODE GOES HERE
+  // var title = document.querySelector('#page-title');
+  // title.innerText = "I'm in a file";
+
+  var todo = document.querySelector('#todo');
+
+  var items = document.querySelectorAll('.item');
+  var before = items[2];
+
+  for (var i = 0; i < 1000; i++) {
+    var li = document.createElement('li');
+    li.innerText = "Add a todo";
+    li.className = "item";
+
+    todo.insertBefore(li, before);
+  }
 })

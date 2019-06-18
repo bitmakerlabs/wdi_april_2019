18c88928c0d1f3d184e4472bede36c447861f0f3
bettymakes <makewithbetty@gmail.com>
2019-06-18 13:23:09 -0400

    JS Events lesson

--- 32-events/instructor/delegate.js
+++ 32-events/instructor/delegate.js
@@ -6,6 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const container = document.querySelector('#container');
 
+  container.addEventListener('click', function(e) {
+    if(e.target.classList.contains('circle')) {
+      e.target.remove();
+    }
+  });
+
   const boxMaker = document.querySelector('#box-maker');
 
   boxMaker.addEventListener('click', function() {
@@ -23,8 +29,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
       circle.innerHTML = count++;
 
+      // WE WON! It worked!!
+      // One consideration: We can potentially have infinite amount of listeners
+      // That's expensive for the browser.
+      // circle.addEventListener('click', function(e) {
+      //   console.log(e);
+      //   e.target.remove();
+      // });
+
       container.append(circle);
 
       console.log('link was clicked');
   });
+
+  // ⚠️ This didn't work because when the page initially loads, we have no circles there.
+  // We need to find another solution that'll add the event listeners to the circles
+  // once the circles are added to the page.
+  // const circles = document.querySelectorAll('.circle');
+  //
+  // circles.forEach(function(circle) {
+  //   circle.addEventListener('click', function(e) {
+  //     console.log("circle is clicked");
+  //     console.log(e);
+  //
+  //     e.remove();
+  //   })
+  // });
 });
--- 32-events/instructor/script.js
+++ 32-events/instructor/script.js
@@ -1,1 +1,65 @@
-// Let's add our first event handlers to this simple document
+document.addEventListener('DOMContentLoaded', function() {
+
+  // 1. Let's add our first event handlers to this simple document
+  const clickBait = document.querySelector('#click-bait');
+
+  // 2. Add an event listener to clickBait and ...
+  //    implement the callback with an anonymous function
+  clickBait.addEventListener('click', function(event) {
+    alert("I've been clicked!");
+
+    event.preventDefault();
+  });
+
+
+  // 3. Select the `second-level` element
+  const secondLevel = document.querySelector('#second-level');
+
+
+  // 4. Add an event listener to secondLevel and ...
+  //    implement the callback with a function definition
+  const handleSecondLevelClick = function(e) {
+    alert('second level clicked!');
+
+    e.stopPropagation();
+  }
+
+  secondLevel.addEventListener('click', handleSecondLevelClick)
+
+
+
+  // 5. Select the `first-level` element
+  const firstLevel = document.querySelector('#first-level');
+
+  // 6. Add an event listener to the firstLevel and ...
+  //    implement the callback with a function declaration
+  function handleFirstLevelClick () {
+    alert('first level is clicked!!!');
+  }
+
+  firstLevel.addEventListener('click', handleFirstLevelClick);
+
+
+
+
+  // 7. Check out the `event` object:
+    // => Google MDN addEventListener
+    // => https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+
+    // QQ: Our default behaviour of the link routes us to Google, can we stop the default behaviour?
+    // (Check out preventDefault)
+
+    // QQ: What default behaviours will you want to prevent typically? There are mainly 2:
+    // (1: links ... 2. _ _ _ _ _? (rhymes with torms lol) )
+
+    // QQ: Can we stop the event from bubbling up the DOM?
+    // => https://developer.mozilla.org/en-US/docs/Web/API/Event/stopPropagation
+
+    // QQ: Does order of including our JavaScript matter?
+    // DOMContentLoaded => https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+
+    // Refrence to all browser events: https://developer.mozilla.org/en-US/docs/Web/Events
+
+    // Example from earlier: https://codepen.io/fightingtheboss/pen/BNajOr
+
+});

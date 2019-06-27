de738ade11cf0956e5066f71d3046959c9928fb2
General Assembly <generalassembly@Generals-iMac.local>
2019-06-27 11:28:54 -0400

    [39-react-part-2] Refactor to be data driven, solve 'each child should have a key prop' warning

--- 39-react-part-2/instructor/colour-palette-react/src/Palette.js
+++ 39-react-part-2/instructor/colour-palette-react/src/Palette.js
@@ -2,11 +2,20 @@ import React from 'react';
 import Swatch from './Swatch';
 
 const Palette = () => {
+
+  const swatches = [
+    { red: 255, green: 0, blue: 0 },
+    { red: 0, green: 255, blue: 0 },
+    { red: 0, green: 0, blue: 255 },
+  ];
+
+  const swatchElements = swatches.map( (s, i) =>
+    <Swatch key={i} {...s} />
+  );
+
   return (
     <ul className="palette">
-      <Swatch red={255} green={0} blue={0} />
-      <Swatch red={0} green={255} blue={0} />
-      <Swatch red={0} green={0} blue={255} />
+      { swatchElements }
     </ul>
   );
 };

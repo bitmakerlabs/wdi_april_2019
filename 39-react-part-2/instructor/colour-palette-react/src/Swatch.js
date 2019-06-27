cc635a49ba97cf3584007b4f2f3f328f7463d45e
General Assembly <generalassembly@Generals-iMac.local>
2019-06-27 12:01:02 -0400

    [39-react-part-2] Add remove swatch button

--- 39-react-part-2/instructor/colour-palette-react/src/Swatch.js
+++ 39-react-part-2/instructor/colour-palette-react/src/Swatch.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Channel from './Channel';
 
-const Swatch = ({red, green, blue}) => {
+const Swatch = ({red, green, blue, onRemove}) => {
 
   let [r, setR] = useState(red);  // ===> [orig_value_of_prop, function_to_change_the_prop]
   let [g, setG] = useState(green);
@@ -13,11 +13,15 @@ const Swatch = ({red, green, blue}) => {
 
   return (
     <li className="swatch" style={style}>
-      <div>rgb(</div>
-      <Channel value={r} handleValueChange={setR} />
-      <Channel value={g} handleValueChange={setG} />
-      <Channel value={b} handleValueChange={setB} />
-      <div>);</div>
+      <div className ="swatch-controls">
+        <div>rgb(</div>
+        <Channel value={r} handleValueChange={setR} />
+        <Channel value={g} handleValueChange={setG} />
+        <Channel value={b} handleValueChange={setB} />
+        <div>);</div>
+      </div>
+
+      <button className="swatch-cta" onClick={onRemove}>Remove</button>
     </li>
   )
 };

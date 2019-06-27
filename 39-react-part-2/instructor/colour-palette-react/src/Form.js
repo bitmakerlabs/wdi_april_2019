aa8c7114a23899ad8ffe7cc49f75d255757e84a8
General Assembly <generalassembly@Generals-iMac.local>
2019-06-27 11:41:14 -0400

    [39-react-part-2] Use useRef to get at input fields of the form

--- 39-react-part-2/instructor/colour-palette-react/src/Form.js
+++ 39-react-part-2/instructor/colour-palette-react/src/Form.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Form = () => {
+
+  const rRef = useRef();
+  const gRef = useRef();
+  const bRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(
+        rRef.current.value, 
+        gRef.current.value, 
+        bRef.current.value
+    );
+  }
+
   return (
-    <form>
-      <input type="number" />
-      <input type="number" />
-      <input type="number" />
+    <form onSubmit={handleSubmit}>
+      <input ref={rRef} type="number" />
+      <input ref={gRef} type="number" />
+      <input ref={bRef} type="number" />
       <button>Add Colour!</button>
     </form>
   );
